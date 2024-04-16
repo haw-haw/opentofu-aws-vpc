f4d56f37573918b351b9bbeb2ecb07ff0e4fc6a3
haw-haw <506263+haw-haw@users.noreply.github.com>
2024-04-16 12:47:03 +0800

    feat: add acocunt info

--- output.tf
+++ output.tf
@@ -33,3 +33,15 @@ output "private_subnets" {
 output "security_groups" {
     value = aws_security_group.allow_base
 }
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
